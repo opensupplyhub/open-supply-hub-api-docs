4ddaee14a02646efe912ab53028350343ed8f910
Stephan Protsack <stephan@speedandfunction.com>
2024-02-20 17:46:58 +0100

    Add elsaticsearch setup

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from elasticsearch import Elasticsearch
+from os import getenv
 
 app = FastAPI()
+search = Elasticsearch(getenv("ELASTICSEARCH_URL"))
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {}
